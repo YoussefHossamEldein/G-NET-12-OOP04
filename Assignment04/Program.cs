c8c5b42ddf2e7433aa65b2f14d98ca8c85e0133f
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-09 16:34:12 +0200

    Theoritical questions

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            /* Static binding is done when there is a parent class and child class 
+             * and there is a method in parent class that is used in the child class 
+             * but with "new" keyowrd so the method of parent is hidden which is called 
+             * method hiding and it occurs in compile time */
+
+
+            /* dynamic binding used with virtual and override of functions when a child 
+             * class override a method from the parent class with its own logic and it 
+             * decide which method to use at run time */
+
+
+
+            #endregion
+
+            #region Question02
+
+            /* method overloading is the method with the same name but different parameters */
+
+            /* method overriding is when a child class override a virtual method from 
+             * the parent class with its own logic */
+            #endregion
+
+            #region Question03
+
+            /* virtual with parent method and override with child method */ 
+            /* virtual means allow this function to be overriden */
+
+            #endregion
+
+
         }
     }
 }
